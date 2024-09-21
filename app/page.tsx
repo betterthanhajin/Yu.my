0602a243c6492e148f43f96c44334b26eb9e2e4f
Hajin Lee <betterthanhajin@icloud.com>
2024-09-22 00:55:48 +0900

    fix: backgorund

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
   }, []);
 
   return (
-    <div className="w-full h-full bg-orange-400">
+    <div className="w-full h-full bg-white">
       <FlowingLiquidEffect tiltX={tiltX} />
     </div>
   );
